88f8ddcea445d2dcca35110af16a4d94751518d4
ArufCuy <77611639+ArufCuy@users.noreply.github.com>
2024-09-15 21:05:51 +0800

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const swiper = new Swiper('.swiper-container', {
-  direction: 'vertical',
-  mousewheel: {},
-  effect: 'cube',
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false
-  }
-});
+
